a5d27a018e3e739cb87fa2bc308f88119bdce511
marinchuck <marincsakt@gmail.com>
2023-02-24 18:37:37 +0100

    updated pom.xml

--- src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
+++ src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
@@ -1,5 +1,7 @@
 package org.sixbodyguards.al;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AkkadianLexiconApplication {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 1; i++) {
-            System.out.println("a");
-        }
         SpringApplication.run(AkkadianLexiconApplication.class, args);
     }
 
