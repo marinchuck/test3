e893f4c98d797622e2ed952b9c96b09f88939a9a
marinchuck <marincsakt@gmail.com>
2023-02-24 19:02:15 +0100

    updated github workflows

--- src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
+++ src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
@@ -1,13 +1,10 @@
 package org.sixbodyguards.al;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AkkadianLexiconApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AkkadianLexiconApplication.class, args);
     }
