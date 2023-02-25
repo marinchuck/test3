1bf55dd602efdc7056aef761ce0f8a2bba56c0a9
marinchuck <marincsakt@gmail.com>
2023-02-25 21:28:09 +0100

    modified tests

--- src/test/java/org/sixbodyguards/al/AkkadianLexiconApplicationTests.java
+++ src/test/java/org/sixbodyguards/al/AkkadianLexiconApplicationTests.java
@@ -10,35 +10,6 @@ class AkkadianLexiconApplicationTests {
   void contextLoads() {}
 
     @Test
-    void test12() {}
-
-    @Test
     void test1() {}
-    @Test
-    void test13() {}
-    @Test
-    void test14() {}
-
-
-    @Test
-    void test15() {}
-    @Test
-    void test16() {}
-    @Test
-    void test18() {}
-    @Test
-    void test17() {}
-    @Test
-    void test19() {}
-    @Test
-    void test20() {}
-    @Test
-    void test121() {}
-    @Test
-    void test124() {}
-
-
-
-
 
 }
