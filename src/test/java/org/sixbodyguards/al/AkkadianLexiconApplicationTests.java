8df2a15647d7a746a56bf25ff8aae4f3162988d6
marinchuck <marincsakt@gmail.com>
2023-02-26 00:03:36 +0100

    modified tests

--- src/test/java/org/sixbodyguards/al/AkkadianLexiconApplicationTests.java
+++ src/test/java/org/sixbodyguards/al/AkkadianLexiconApplicationTests.java
@@ -6,19 +6,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AkkadianLexiconApplicationTests {
 
-  @Test
-  void contextLoads() {}
-
-  @Test
-  void test12() {}
-
-  @Test
-  void test152() {}
-
-  @Test
-  void test153() {
-    for (; ; ) {
-      break;
+    @Test
+    void contextLoads() {
     }
-  }
+
+    @Test
+    void test12() {
+    }
+
+    @Test
+    void test152() {
+    }
+
+    @Test
+    void test153() {
+        for (; ; ) {
+            break;
+        }
+    }
 }
