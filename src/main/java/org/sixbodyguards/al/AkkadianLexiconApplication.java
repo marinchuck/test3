adf220be1e1aa35f838461918120abcd2ee37a0a
marinchuck <marincsakt@gmail.com>
2023-02-26 00:02:39 +0100

    modified pom

--- src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
+++ src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AkkadianLexiconApplication {
 
-    public static void main(String[] args) {
-        // alma
-        System.out.println("alma");
-        SpringApplication.run(AkkadianLexiconApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    // alma
+    System.out.println("alma");
+    SpringApplication.run(AkkadianLexiconApplication.class, args);
+  }
 }
--- src/test/java/org/sixbodyguards/al/AkkadianLexiconApplicationTests.java
+++ src/test/java/org/sixbodyguards/al/AkkadianLexiconApplicationTests.java
@@ -6,26 +6,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AkkadianLexiconApplicationTests {
 
-    @Test
+  @Test
+  void contextLoads() {}
 
-    void contextLoads() {
+  @Test
+  void test12() {}
+
+  @Test
+  void test152() {}
+
+  @Test
+  void test153() {
+    for (; ; ) {
+      break;
     }
-
-    @Test
-    void test12() {
-
-    }
-
-    @Test
-    void test152() {
-
-    }
-
-    @Test
-    void test153() {
-        for (;;) {
-            break;
-        }
-    }
-
+  }
 }
