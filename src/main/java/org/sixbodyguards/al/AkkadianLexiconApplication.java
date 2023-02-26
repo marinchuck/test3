22f5c8d212190d4d0009988fc689c290e9de85a9
marinchuck <marincsakt@gmail.com>
2023-02-26 10:19:42 +0100

    conflict?

--- src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
+++ src/main/java/org/sixbodyguards/al/AkkadianLexiconApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AkkadianLexiconApplication {
 
   public static void main(String[] args) {
+    //dev7
     SpringApplication.run(AkkadianLexiconApplication.class, args);
   }
 }
